d989ab2fc29e721357ffa1b25d63c20e93e0449e
AzureAD\BrunaSergio <bruna.sergio@mslsolutions.com>
2021-11-27 12:29:23 +1000

    cards

--- src/components/Canvas/CanvasCard.jsx
+++ src/components/Canvas/CanvasCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import './CanvasCard.css'
 
 function CanvasCard({ projectData, isEditMode = false }) {
     // const token = window.localStorage.getItem("token");
@@ -8,16 +9,16 @@ function CanvasCard({ projectData, isEditMode = false }) {
     //     return fetch(`${process.env.REACT_APP_API_URL}canvas/${id}`, { method: 'delete', "Authorization": `Token ${token}` }).then(r => r.json())
     // }
     return (
-        <div className="card-wrapper">
-            <div className="project-card">
-                <Link to={`/canvas/${projectData.id}`}>
-                    <img src={projectData.image} alt="canvas " />
-                    <h3 className="project-title--card">{projectData.title}</h3>
-
-                </Link>
-            </div>
-
-        </div>);
+        <div className="canvas-card">
+            <Link to={`/canvas/${projectData.id}`}>
+                <img src={projectData.image} alt="canvas " />
+                <div className="card-inner-container">
+                    <h3 className="canvas-title--card">{projectData.title}</h3>
+                    <p className="canvas-team--card">{projectData.team}</p>
+                </div>
+            </Link>
+        </div>
+    );
 }
 
 export default CanvasCard;

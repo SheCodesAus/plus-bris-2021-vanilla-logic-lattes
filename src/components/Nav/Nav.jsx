dd423e0f88b9229793c55148b27bfaf982fa6337
AzureAD\BrunaSergio <bruna.sergio@mslsolutions.com>
2021-11-30 19:12:00 +1000

    nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -26,40 +26,44 @@ const Nav = () => {
         <Link className="logo" to="/">
           <img src={logo} alt="logo" className="img-logo" />
         </Link>
-        {/* </nav><div className={`nav-links ${navbarOpen ? " nav-active" : ""}`}> */}
-        {isLoggedIn && (
-          <Link className="button" to="/">
-            Home
+        <div className={`nav-items ${navbarOpen ? " nav-active" : ""}`}>
+          {isLoggedIn && (
+            <Link className="button" to="/">
+              Home
+            </Link>
+          )}
+          <Link className="button" to="/about">
+            About
           </Link>
-        )}
-        <Link className="button" to="/about">
-          About
-        </Link>
-        {!isLoggedIn && (
-          <Link className="button" to="/login">
-            Login
-          </Link>
-        )}
-        {isLoggedIn && (
-          <Link onClick={logout} className="button" to="/login">
-            Logout
-          </Link>
-        )}
+          {!isLoggedIn && (
+            <Link className="button" to="/login">
+              Login
+            </Link>
+          )}
+          {isLoggedIn && (
+            <Link onClick={logout} className="button" to="/login">
+              Logout
+            </Link>
+          )}
 
-        {!isLoggedIn && (
-          <Link className="button" to="/register">
-            Register
-          </Link>
-        )}
-        <div className={`burger`} onClick={navSlide}>
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
+          {!isLoggedIn && (
+            <Link className="button" to="/register">
+              Register
+            </Link>
+          )}
+        </div>
+        <div className="responsive-nav">
+          <div className={`burger`} onClick={navSlide}>
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
+          {isLoggedIn && <div className="username--nav_resp">Hi, {username}</div>}
         </div>
       </nav>
       {isLoggedIn && <div className="username--nav">Hi, {username}</div>}
       <div id="center">
-        <img className="center" src={sprinkles} alt="logo" />
+        <img className="center" id="sprinkles-logo" src={sprinkles} alt="logo" />
       </div>
     </div>
   );
